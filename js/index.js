4d585f63e2ca3323c22688a1c74ac8544ca6536b
mari-no <marin.nosova@gmail.com>
2026-05-25 11:40:54 -0500

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -1,9 +1,79 @@
 
+
+let messageSection = document.querySelector("#messages");
+let messageList = messageSection.querySelector("ul");
+
 let today = new Date();
 let thisYear = today.getFullYear();
 let footer = document.createElement('footer');
 let copyright = document.createElement("p");
 let body = document.querySelector("body");
+
+let messageForm = document.querySelector('[name="leave_message"]');
+
+messageForm.addEventListener("submit", function(event){
+event.preventDefault();
+let newMessage = document.createElement("li");
+let usersName =  event.target.usersName.value;
+let usersEmail = event.target.usersEmail.value;
+let usersMessage = event.target.usersMessage.value;
+
+console.log(usersName,usersEmail, usersMessage);
+
+newMessage.innerHTML = `<a href = " mailto: ${usersEmail}">  ${usersName} </a>
+                        <span> ${usersMessage} </span>`;
+let editButton = document.createElement("button");
+editButton.innerText = "edit";
+editButton.type = "button";
+editButton.addEventListener("click", function(){
+    if (editButton.textContent === "edit"){let entry = editButton.parentNode;
+    let editedText = entry.querySelector("span");
+    console.log(editedText);
+    const input = document.createElement("input");
+    input.type = "text";
+    input.value = editedText.textContent;
+    entry.insertBefore(input, editedText);
+    console.log(input);
+    entry.removeChild(editedText);
+    editButton.textContent = "save";}
+    else if (editButton.textContent === "save"){
+        let entry = editButton.parentNode;
+        const input = entry.querySelector("input");
+        console.log(input);
+        const span = document.createElement("span");
+        span.textContent = input.value;
+        entry.insertBefore(span, input);
+        entry.removeChild(input);
+        editButton.textContent = "edit";
+    } 
+  
+})
+
+let removeButton = document.createElement("button");
+removeButton.innerText =  "remove";
+removeButton.type = "button";
+removeButton.addEventListener("click", function(){
+    let entry = removeButton.parentNode;
+    entry.remove();
+    hideMessages();
+   
+})
+newMessage.appendChild(removeButton);
+newMessage.appendChild(editButton);
+messageList.appendChild(newMessage);
+hideMessages();
+event.target.reset();
+})
+let messagesList = document.querySelector("#messages ul"); 
+let messages = document.getElementById("messages");
+function hideMessages(){
+    if (messagesList.childElementCount === 0){
+    messages.style.display = "none";
+}else{
+    messages.style.display = "block";
+}
+}
+hideMessages();
 copyright.innerHTML = `&copy; ${thisYear} Marina Nosova`;
 body.appendChild(footer);
 footer.appendChild(copyright);
